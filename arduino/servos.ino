d5b251fb99e666eef05fdfd1227fb6fc2af5a5a4
petres <peter.reschenhofer@gmail.com>
2017-12-04 21:52:26 +0100

    Makefile added

--- arduino/servos.ino
+++ arduino/servos.ino
@@ -4,86 +4,92 @@
 
 const int servoCount = 4;
 
-Servo servo[servoCount];    
+Servo servo[servoCount];
 int pins[servoCount] = {9, 10, 11, 12};
 
 const byte numChars = 16;
-char receivedChars[numChars]; 
-                
+char receivedChars[numChars];
+
 boolean gotNewLine = false;
- 
+
+void recvTillNewLine();
+void processNewLine(char *);
+
+
 void setup() {
-  Serial.begin(2000000);
-  for(int i = 0; i < servoCount; i += 1) {
-    servo[i].attach(pins[i]);  
-  }
-  Serial.println("Ready!");
-} 
-  
+    Serial.begin(2000000);
+    for(int i = 0; i < servoCount; i += 1)
+        servo[i].attach(pins[i]);
+    
+    #if DEBUG
+    Serial.println("Ready!");
+    #endif
+}
+
 
 void loop() {
-  recvTillNewLine();
-  if (gotNewLine) 
-    processNewLine(receivedChars);
-    gotNewLine = false;
+    recvTillNewLine();
+    if (gotNewLine) {
+        processNewLine(receivedChars);
+        gotNewLine = false;
+    }
 }
 
 void recvTillNewLine() {
-  static byte ndx = 0;
-  char endMarker = '\n';
-  char rc;
-  
-  while (Serial.available() > 0 && gotNewLine == false) {
-    rc = Serial.read();
-    if (rc != endMarker) {
-      receivedChars[ndx] = rc;
-      ndx++;
-      if (ndx >= numChars) {
-        ndx = numChars - 1;
-      }
-    } else {
-      receivedChars[ndx] = '\0';
-      ndx = 0;
-      gotNewLine = true;
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+
+    while (Serial.available() > 0 && gotNewLine == false) {
+        rc = Serial.read();
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars)
+                ndx = numChars - 1;
+        } else {
+            receivedChars[ndx] = '\0';
+            ndx = 0;
+            gotNewLine = true;
+        }
     }
-  }
 }
 
 void setServoPosition(int servoId, int position) {
-  if (servoId < servoCount && position >= 0 && position <= 180) {
-    #if DEBUG
-    Serial.print("Set Servo ");
-    Serial.print(servoId);
-    Serial.print(" to ");
-    Serial.print(position);
-    Serial.println("."); 
-    #endif
-    
-    servo[servoId].write(position);
-  } else {
-    #if DEBUG
-    Serial.println("Invalid values!"); 
-    #endif
-  }
+    if (servoId < servoCount && position >= 0 && position <= 180) {
+        #if DEBUG
+        Serial.print("Set Servo ");
+        Serial.print(servoId);
+        Serial.print(" to ");
+        Serial.print(position);
+        Serial.println(".");
+        #endif
+
+        servo[servoId].write(position);
+    } else {
+        #if DEBUG
+        Serial.println("Invalid values!");
+        #endif
+    }
 }
 
 void processNewLine(char * receivedChars) {
-  #if DEBUG
-  Serial.print("GOT: '");
-  Serial.print(receivedChars);
-  Serial.println("'");
-  #endif
-  char* command = strtok(receivedChars, " ");
-  while (command != 0) {
-      char* separator = strchr(command, ':');
-      if (separator != 0) {
-          *separator = 0;
-          int servoId = atoi(command);
-          ++separator;
-          int position = atoi(separator);
+    #if DEBUG
+    Serial.print("GOT: '");
+    Serial.print(receivedChars);
+    Serial.println("'");
+    #endif
+    char* command = strtok(receivedChars, " ");
+    while (command != 0) {
+        char* separator = strchr(command, ':');
+        if (separator != 0) {
+            *separator = 0;
+            int servoId = atoi(command);
+            ++separator;
+            int position = atoi(separator);
 
-          setServoPosition(servoId, position);
-      }
-      command = strtok(0, " ");
-  }
+            setServoPosition(servoId, position);
+        }
+        command = strtok(0, " ");
+    }
 }
